b138ad2fdae3d9b0405a7d6c10523265d686c2bb
mailsondias7 <mailsondias7@gmail.com>
2020-01-17 16:42:20 -0300

    Criando Front-End com REact

---
+++ web/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './global.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-blocks">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="username_github" required />
+          </div>
+
+          <div class="input-blocks">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-blocks">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
